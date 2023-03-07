7513380161b6615083d18065930ff68d06ed5431
Robin <rorbbin@gmail.com>
2023-03-07 22:39:42 +0800

    Add request headers.

--- realtime_quote.py
+++ realtime_quote.py
@@ -3,6 +3,13 @@ import requests
 
 def get_realtime_quote_price(symbol):
     url = f"https://finance.yahoo.com/quote/{symbol}?p={symbol}&.tsrc=fin-srch"
+
+    h = {
+        "Cache-Control": "no-cache",
+        "Pragma": "no-cache",
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+    }
+
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'lxml')
     price = soup.find('fin-streamer', attrs={"data-symbol":f"{symbol}", "data-field":"regularMarketPrice"})
