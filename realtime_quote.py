5f2d14f43ef9fa7d74353cec80266eae2747a698
Robin <rorbbin@gmail.com>
2023-03-07 23:22:50 +0800

    Fix error.

--- realtime_quote.py
+++ realtime_quote.py
@@ -18,4 +18,4 @@ def get_realtime_quote_price(symbol):
 import yfinance as yf
 def get_realtime_quote_price(symbol):
     data = yf.download(tickers='QQQ', period='1d', interval='1m')
-    print(data.values[-1][0])
+    return data.values[-1][0]
