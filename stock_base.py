0b4606ecb06d9ad0d098565cb9911e528eb74213
Robin Lan <rorbbin@gmail.com>
2023-03-13 14:46:03 +0800

    Change format.

--- stock_base.py
+++ stock_base.py
@@ -1,14 +1,13 @@
 import abc
 import orjson
-import re
 from enum import Enum
 from typing import List
 from file_util import ensure_path_exists
 import env
-import logging
 from datetime import date
 import realtime_quote
 import os.path
+
 
 class OptionType(Enum):
     NONE = 0
@@ -16,13 +15,15 @@ class OptionType(Enum):
     PUT = 2
     BOTH = 3
 
+
 class SecurityType(Enum):
     ALL = 0
-    STK = 1 # stock
-    OPT = 2 # option
+    STK = 1  # stock
+    OPT = 2  # option
+
 
 class StockOption:
-    def __init__(self, symbol:str, id:str, type:OptionType, bid:float, ask:float, strike:float, expire_date:str) -> None:
+    def __init__(self, symbol: str, id: str, type: OptionType, bid: float, ask: float, strike: float, expire_date: str) -> None:
         self.Symbol = symbol
         self.Id = id
         self.Type = type
@@ -31,24 +32,30 @@ class StockOption:
         self.Strike = strike
         self.Expiry = expire_date
 
+
 class OrderType(Enum):
     MarketOrder = 0
     LimitOrder = 1
+
 
 class OrderSide(Enum):
     BUY = 0
     SELL = 1
 
+
 class OrderOpenClose(Enum):
     OPEN = 0
     CLOSE = 1
+
 
 class OrderTTL(Enum):
     GTC = 0
     DAY = 1
 
+
 class OrderMarket(Enum):
     US = 0
+
 
 class OrderStatusType(Enum):
     EXPIRED = 0
@@ -60,17 +67,18 @@ class OrderStatusType(Enum):
     REJECTED = 6
     UNKNOWN = -1
 
+
 class Order:
-    def __init__(self, id:str,
-                 symbol:str,
+    def __init__(self, id: str,
+                 symbol: str,
                  type: OrderType,
-                 side:OrderSide,
-                 open_close:OrderOpenClose,
-                 ttl:OrderTTL,
-                 market:OrderMarket,
-                 price:float,
-                 quantity:int,
-                 sec_type:SecurityType
+                 side: OrderSide,
+                 open_close: OrderOpenClose,
+                 ttl: OrderTTL,
+                 market: OrderMarket,
+                 price: float,
+                 quantity: int,
+                 sec_type: SecurityType
                  ) -> None:
         self.Id = id
         self.Symbol = symbol
@@ -83,21 +91,22 @@ class Order:
         self.Quantity = quantity
         self.SecurityType = sec_type
 
+
 class StockPosition:
     def __init__(self,
-                 Account:str,
-                 Exchange:str,
-                 Symbol:str,
-                 Id:str,
-                 AverageCost:float,
-                 Quantity:int,
-                 SecurityType:SecurityType,
-                 OptionType:OptionType,
-                 TradingDate:str,
-                 MarketValue:float,
-                 MarketPrice:float,
-                 Expiry:str,
-                 Strike:float) -> None:
+                 Account: str,
+                 Exchange: str,
+                 Symbol: str,
+                 Id: str,
+                 AverageCost: float,
+                 Quantity: int,
+                 SecurityType: SecurityType,
+                 OptionType: OptionType,
+                 TradingDate: str,
+                 MarketValue: float,
+                 MarketPrice: float,
+                 Expiry: str,
+                 Strike: float) -> None:
         self.Account = Account
         self.Exchange = Exchange
         self.Symbol = Symbol
@@ -112,13 +121,15 @@ class StockPosition:
         self.Expiry = Expiry
         self.Strike = Strike
 
+
 class OrderOperationResult:
-    def __init__(self, error_id:str, error_msg:str) -> None:
+    def __init__(self, error_id: str, error_msg: str) -> None:
         self.ErrorId = error_id
         self.ErrorMsg = error_msg
 
+
 class OrderStatus(abc.ABC):
-    def __init__(self,order) -> None:
+    def __init__(self, order) -> None:
         self.brokerOrder = order
 
     @abc.abstractmethod
@@ -141,115 +152,121 @@ class OrderStatus(abc.ABC):
     def get_order_remaining(self) -> int:
         '''pass'''
 
+
 class IStockClient(abc.ABC):
 
     @abc.abstractmethod
-    def initialize(self, prod_env:bool, account:str, symbol:str):
+    def initialize(self, prod_env: bool, account: str, symbol: str):
         '''Intialize the client.'''
-    
+
     @abc.abstractmethod
-    def get_option_chain(self, symbol:str, expire_date_str:str, type:OptionType) -> List[StockOption]:
+    def get_option_chain(self, symbol: str, expire_date_str: str, type: OptionType) -> List[StockOption]:
         '''Get the symbol's option chain with expected type and expire date. Sorted by strike asc.'''
 
     @abc.abstractmethod
-    def get_position(self, market: OrderMarket, security_type: SecurityType, symbol:str) -> List[StockPosition]:
+    def get_position(self, market: OrderMarket, security_type: SecurityType, symbol: str) -> List[StockPosition]:
         '''Get the positions in target market.'''
 
     @abc.abstractmethod
-    def place_order(self, order:Order) -> OrderOperationResult:
+    def place_order(self, order: Order) -> OrderOperationResult:
         '''Place an self.'''
 
     @abc.abstractmethod
-    def modify_order(self, order_id:str, new_quantity:int, new_price:float) -> OrderOperationResult:
+    def modify_order(self, order_id: str, new_quantity: int, new_price: float) -> OrderOperationResult:
         '''Modify an self.'''
-    
+
     @abc.abstractmethod
-    def cancel_order(self, order_id:str) -> OrderOperationResult:
+    def cancel_order(self, order_id: str) -> OrderOperationResult:
         '''Modify an self.'''
-    
+
     @abc.abstractmethod
-    def query_order(self, order_id:str) -> OrderStatus:
+    def query_order(self, order_id: str) -> OrderStatus:
         '''Query an self.'''
 
     @abc.abstractmethod
-    def buy_option_to_close(self, id:str, opt_type:OptionType, quantity:int) -> OrderStatus:
+    def buy_option_to_close(self, id: str, opt_type: OptionType, quantity: int) -> OrderStatus:
         '''Query an self.'''
 
     @abc.abstractmethod
-    def sell_put_option_to_open(self, symbol:str, strike:float, quantity:int, expired_date:date) -> OrderStatus:
-        '''Query an self.'''
-    
-    @abc.abstractmethod
-    def sell_position_to_close(self, opt_position:StockPosition) -> OrderStatus:
+    def sell_put_option_to_open(self, symbol: str, strike: float, quantity: int, expired_date: date) -> OrderStatus:
         '''Query an self.'''
 
     @abc.abstractmethod
-    def sell_stock_to_close(self, symbol:str, quantity:int) -> OrderStatus:
+    def sell_position_to_close(self, opt_position: StockPosition) -> OrderStatus:
         '''Query an self.'''
 
     @abc.abstractmethod
-    def sell_all_stock_to_close(self, symbol:str) -> OrderStatus:
+    def sell_stock_to_close(self, symbol: str, quantity: int) -> OrderStatus:
         '''Query an self.'''
-    
+
     @abc.abstractmethod
-    def get_option_position(self, market: OrderMarket, symbol:str, optionType:OptionType, expiry:date) -> List[StockPosition]:
+    def sell_all_stock_to_close(self, symbol: str) -> OrderStatus:
         '''Query an self.'''
-    
+
     @abc.abstractmethod
-    def sell_option_with_protection_to_open(self, symbol:str, opt_type: OptionType,strike:float, quantity:int, expired_date:date, protect_times:float) -> OrderStatus:
+    def get_option_position(self, market: OrderMarket, symbol: str, optionType: OptionType, expiry: date) -> List[StockPosition]:
+        '''Query an self.'''
+
+    @abc.abstractmethod
+    def sell_option_with_protection_to_open(self, symbol: str, opt_type: OptionType, strike: float, quantity: int, expired_date: date, protect_times: float) -> OrderStatus:
         '''Query an self.'''
 
     @abc.abstractmethod
     def get_account_id(self) -> str:
         '''Get order id.'''
-    
+
     @abc.abstractmethod
-    def get_open_option_orders(self, market: OrderMarket, symbol:str, opt_type: OptionType, expired_date:date) -> list:
+    def get_open_option_orders(self, market: OrderMarket, symbol: str, opt_type: OptionType, expired_date: date) -> list:
         '''Get the open orders.'''
-        
-def get_option_type_from_str(opt_type:str) -> OptionType:
+
+
+def get_option_type_from_str(opt_type: str) -> OptionType:
     if opt_type.casefold() == "PUT".casefold():
         return OptionType.PUT
     elif opt_type.casefold() == "CALL".casefold():
         return OptionType.CALL
     else:
         return OptionType.NONE
-    
 
-def save_positions_to_file(expired_str:str, account:str, symbol:str, positions:List[StockPosition]) -> bool:
+
+def save_positions_to_file(expired_str: str, account: str, symbol: str, positions: List[StockPosition]) -> bool:
     filename = get_positions_local_file_name(expired_str, account, symbol)
     with open(filename, 'wb') as f:
-        json_str = orjson.dumps(positions,  default = lambda x: x.__dict__)
+        json_str = orjson.dumps(positions,  default=lambda x: x.__dict__)
         f.write(json_str)
     return True
 
-def load_positions_from_file(expired_str:str, account:str, symbol:str) -> List[StockPosition]:
+
+def load_positions_from_file(expired_str: str, account: str, symbol: str) -> List[StockPosition]:
     objs = []
     filename = get_positions_local_file_name(expired_str, account, symbol)
     if not os.path.isfile(filename):
         return objs
-    
+
     with open(filename, 'rb') as f:
         objs = orjson.loads(f.read())
     positions = []
     for o in objs:
         positions.append(StockPosition(**o))
-    
+
     return positions
 
-def get_positions_local_file_name(expired_date_str:str, account:str, symbol:str) -> str:
+
+def get_positions_local_file_name(expired_date_str: str, account: str, symbol: str) -> str:
     path = f"{env.get_data_root_path()}/position/"
     ensure_path_exists(path)
     return f"{path}/{expired_date_str}_{account}_{symbol}_opt.json"
 
-def get_put_option_strike_price(symbol:str) -> float:
+
+def get_put_option_strike_price(symbol: str) -> float:
     return realtime_quote.get_realtime_quote_price(symbol) * (1 + __get_stock_miu(symbol))
 
-def get_contract_number_of_option(symbol:str) -> int:
+
+def get_contract_number_of_option(symbol: str) -> int:
     return 2
 
 
-def __get_stock_miu(symbol:str):
+def __get_stock_miu(symbol: str):
     if symbol.upper() == "SPY":
         return 0.130108/100
     elif symbol.upper() == "QQQ":
