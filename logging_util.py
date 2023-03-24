047701bacebecd3261ca468ec01326cbb560000e
Robin <rorbbin@gmail.com>
2023-03-25 07:42:41 +0800

    Fix log level to debug.

--- logging_util.py
+++ logging_util.py
@@ -9,7 +9,7 @@ def setup_logging(fileName):
     file_util.ensure_path_exists(logPath)
     time_rotating_file_handler = logging.handlers.TimedRotatingFileHandler(filename= logPath +"/"+fileName + '.log', when='D', interval=1, backupCount=7, encoding='utf-8')
     time_rotating_file_handler.setLevel(logging.DEBUG)
-    logging.basicConfig(level=logging.INFO,
+    logging.basicConfig(level=logging.DEBUG,
                 format='%(asctime)s %(levelname)s %(message)s',
                 datefmt='%Y-%m-%d %H:%M:%S',
                 # filename=file_name,
