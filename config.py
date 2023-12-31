69af4919b3f562f78c180667f6514a24e94dc425
Robin <rorbbin@gmail.com>
2023-12-31 09:24:54 +0800

    Update calendar for 2026

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 #Special weekly end date. If the Friday is holiday, the Thursday is the week end date.
 # And usually, Thursday market close a bit earlier.
 # Get from https://www.nyse.com/markets/hours-calendars
-# key=Date, value= (MarketCloseHour', IsWeekEnd, IsMarketOpenToday, EarlyMarketCloseToday)
+# key=Date, value= (MarketCloseHour, IsWeekEnd, IsMarketOpenToday, EarlyMarketCloseToday)
 SPECIAL_WEEK_END_DATE = {
     # Good Friday
     "2023-04-06": (16, True, True, False), # Thursday.
@@ -11,10 +11,12 @@ SPECIAL_WEEK_END_DATE = {
     "2024-03-29": (16, True, False, False), # Friday
     "2025-04-17": (16, True, True, False), # Thursday.
     "2025-04-18": (16, True, False, False), # Friday
+    "2026-04-03": (16, True, False, False), # Friday
     # End Good Friday
 
     # Juneteenth National Independence Day
     "2025-06-19": (16, False, False, False), # Thursday
+    "2026-06-19": (16, False, False, False), # Thursday
     # End Juneteenth National Independence Day
 
     # Independence Day
@@ -24,6 +26,8 @@ SPECIAL_WEEK_END_DATE = {
     "2024-07-04": (13, False, False, False), # Thursday
     "2025-07-03": (13, True, True, True), # Thursday
     "2025-07-04": (16, True, False, False), # Friday
+    "2026-07-02": (16, True, False, False), # Friday
+    "2026-07-03": (16, True, False, False), # Friday
     # End Independence Day
 
     # Thanksgiving Day
@@ -33,11 +37,14 @@ SPECIAL_WEEK_END_DATE = {
     "2024-11-29": (13, True, True, True), # Friday
     "2025-11-27": (16, False, False, False), # Thursday
     "2025-11-28": (13, True, True, True), # Friday
+    "2026-11-26": (16, False, False, False), # Thursday
+    "2026-11-27": (13, True, True, True), # Friday
     # End Thanksgiving Day
 
     # Christmas Day
     "2024-12-24": (13, False, True, True), # Tuesday
     "2025-12-24": (13, False, True, True), # WednesDay
+    "2026-12-24": (13, False, True, True), # Thursday
     # End Christmas Day
 }
 
@@ -76,5 +83,15 @@ MARKET_CLOSE_DATES = {
     "2025-07-04",
     "2025-09-01",
     "2025-11-27",
-    "2025-12-25"
+    "2025-12-25",
+    "2026-01-01",
+    "2026-01-19",
+    "2026-02-16",
+    "2026-04-03",
+    "2026-05-25",
+    "2026-06-19",
+    "2026-07-03",
+    "2026-09-07",
+    "2026-11-26",
+    "2026-12-25"
 }
